5905cd272c6e9a8a5ba7e76ab9a1e36943d6abcf
qihengsan89 <132972782+qihengsan89@users.noreply.github.com>
2023-09-21 14:12:36 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dg.sxoa.top",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
